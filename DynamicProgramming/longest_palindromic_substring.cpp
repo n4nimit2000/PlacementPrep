752a11bd52a6b4afcbab2a277f952a13f6df7518
n4nimit2000 <n4nimit2000@gmail.com>
2024-01-04 14:09:22 +0530

    Add Longest Palindromic Substring Without DP logic

--- DynamicProgramming/longest_palindromic_substring.cpp
+++ DynamicProgramming/longest_palindromic_substring.cpp
@@ -5,7 +5,72 @@ using namespace std;
 #define ll long long int
 #define vll vector<ll>
 
-string longest_palindromic_substring(string s)
+/**
+ * Time Complexity = O(N^2), Space Complexity = O(1)
+ **/
+string longest_palindromic_substring_without_dp(string s)
+{
+    ll n = s.size();
+    if (n == 0)
+    {
+        return "";
+    }
+
+    ll max_len = 1, start_ind = 0;
+    for (int mid = 0; mid < n; mid++)
+    {
+        ll dis = 1, len = 1, tmp_start = 0;
+        // To get odd length palindrome
+        while (mid - dis >= 0 && mid + dis < n)
+        {
+            if (s[mid - dis] == s[mid + dis])
+            {
+                tmp_start = mid - dis;
+                dis++;
+                len += 2;
+
+                if (len > max_len)
+                {
+                    max_len = len;
+                    start_ind = tmp_start;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        dis = 0, len = 0;
+        // To get even length palindrome
+        while (mid - dis >= 0 && mid + 1 + dis < n)
+        {
+            if (s[mid - dis] == s[mid + 1 + dis])
+            {
+                tmp_start = mid - dis;
+                dis++;
+                len += 2;
+
+                if (len > max_len)
+                {
+                    max_len = len;
+                    start_ind = tmp_start;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    return s.substr(start_ind, max_len);
+}
+
+/**
+ * Time Complexity = O(N^2), Space Complexity = O(N^2)
+ **/
+string longest_palindromic_substring_using_dp(string s)
 {
     ll n = s.size();
     if (n == 0)
@@ -43,20 +108,23 @@ int main()
     string s;
     cin >> s;
 
-    cout << "Longest Palindromic Substring: " << longest_palindromic_substring(s) << "\n";
+    cout << "Longest Palindromic Substring using DP: " << longest_palindromic_substring_using_dp(s) << "\n";
+    cout << "Longest Palindromic Substring without DP: " << longest_palindromic_substring_without_dp(s) << "\n";
 
     return 0;
 }
 
 /*
 Given a string s, return the longest palindromic substring in s.
-Time complexity = O(n^2)
-Space complexity = O(n^2)
 
 Sample Input:
 forgeeksskeegfor
 
 Sample Output:
 Longest Palindromic Substring: geeksskeeg
+
+Sample Problems:
+ - https://leetcode.com/problems/longest-palindromic-substring
+ - https://leetcode.com/problems/palindromic-substrings
 
 */
